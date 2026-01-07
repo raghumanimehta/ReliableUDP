2741aa1f740d7f22ecf1ee8ee8a760a24bcd89f5
Raghumani Mehta <raghumani2003@outlook.com>
2026-01-06 19:00:50 -0800

    scaffolding for sliding window

---
+++ slidingWindow.hpp
@@ -0,0 +1,18 @@
+#ifndef SLIDING_WINDOW_HPP
+#define SLIDING_WINDOW_HPP
+
+#include <cstdint>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+#include "packet.hpp"
+
+constexpr size_t WINDOW_SIZE = 10;
+
+struct WindowSlot {
+    
+};
+
+struct SlidingWindow {
+    
+};
